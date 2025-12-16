f3f8f9977fb9e65685d20901496495d784f451b4
吉田光希 <yoshidamitsuki@wire-o018.wire-o.huhs.ac.jp>
2025-12-16 16:36:26 +0900

    フッタページのリンクとページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,9 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  get "/terms",   to: "pages#terms"
+  get "/privacy", to: "pages#privacy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
